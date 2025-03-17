e613ae8ee502eee6d71b77422043ab04c57ba8c7
joseCallisayaSuarez <callisayafer8@gmail.com>
2025-03-17 10:54:55 -0400

    SegundoCommit EjerSuma.py
    Se agrego la multiplicacion y la division

--- EjerSuma.py
+++ EjerSuma.py
@@ -1,4 +1,10 @@
-n=int(input())
-m=int(input())
-s=n+m
-print(s)
+n = int(input())
+m = int(input())
+suma = n + m
+resta = n - m
+multiplicacion = n * m
+division = n / m if m != 0 else "Indefinido"
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
